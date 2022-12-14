d7507aa36f3336fdeb600e697801b018b3e8da7e
Lucas-183119 <lucas.183119@hastudents.us>
2022-12-13 17:57:18 -0700

    Added basic darkmode

--- js/pages.js
+++ js/pages.js
@@ -2,43 +2,49 @@ ready(function (){
 	
 	pageBody = document.getElementById("pageBody");
 	pages = {}
-	pages.homepage = function() {
+	pages.homepage = function() { /*homepage template*/
 		page = document.getElementsByClassName("homepage template")[0].cloneNode(true);
 		page.classList.remove("template");
 		pageBody.textContent = "";
 		pageBody.appendChild(page);
 	}
-	pages.volunteer = function() {
+	pages.volunteer = function() { /*volunteer template*/
 		page = document.getElementsByClassName("volunteer template")[0].cloneNode(true);
 		page.classList.remove("template");
 		pageBody.textContent = "";
 		pageBody.appendChild(page);
 	}
-	pages.about = function() {
+	pages.about = function() { /*about template*/
 		page = document.getElementsByClassName("about template")[0].cloneNode(true);
 		page.classList.remove("template");
 		pageBody.textContent = "";
 		pageBody.appendChild(page);
 	}
-	pages.videos = function() {
+	pages.videos = function() { /*videos template*/
 		page = document.getElementsByClassName("videos template")[0].cloneNode(true);
 		page.classList.remove("template");
 		pageBody.textContent = "";
 		pageBody.appendChild(page);
 	}
-	pages.teams = function() {
+	pages.teams = function() { /*teams template*/
 		page = document.getElementsByClassName("teams template")[0].cloneNode(true);
 		page.classList.remove("template");
 		pageBody.textContent = "";
 		pageBody.appendChild(page);
 	}
-	pages.donations = function() {
+	pages.donations = function() { /*donations template*/
 		page = document.getElementsByClassName("donations template")[0].cloneNode(true);
 		page.classList.remove("template");
 		pageBody.textContent = "";
 		pageBody.appendChild(page);
 	}
 	
+	function drkmdtoggle(event) {
+		document.body.classList.toggle("drkmd")
+	}
+	
+	document.getElementById("drkmdbtn").addEventListener("click", drkmdtoggle );
+		
 	function onRedir () {
 		if (location.hash === "#volunteer") pages.volunteer();
 		else if (location.hash === "#about") pages.about();
