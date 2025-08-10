e111b09fbbf5897f8af5ea433f9ac6350e468756
jbfawcett24 <jbfawcett24@gmail.com>
2025-08-09 17:52:32 -0700

    quick fixes

--- app/src/firebase.ts
+++ app/src/firebase.ts
@@ -1,19 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAebfSk5rUu0Yyl-vtiq4ed17PtZGZOnq4",
-    authDomain: "herobotix-email-service.firebaseapp.com",
-    projectId: "herobotix-email-service",
-    storageBucket: "herobotix-email-service.firebasestorage.app",
-    messagingSenderId: "972248405749",
-    appId: "1:972248405749:web:896cae0182bf41f6f957bc"
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
